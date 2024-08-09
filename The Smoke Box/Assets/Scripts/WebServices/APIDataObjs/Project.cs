8d23fb3aa7572a9e394711d0327486729f18424f
nathanr08 <nream08@gmail.com>
2024-08-09 17:28:39 -0500

    added scene for Web Requests

---
+++ The Smoke Box/Assets/Scripts/WebServices/APIDataObjs/Project.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Project
+{
+    public int ProjectID { get; set; }
+    public string OwnerName { get; set; }
+    public string Name { get; set; }
+    public DateTime LastModified { get; set; }
+    public string FileName { get; set; }
+}
